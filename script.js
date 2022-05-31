d90bd38a6dda42a8f0e4749c9f2b7ff274e9f28f
ProteinK <konprotein@gmail.com>
2022-05-31 14:29:42 +0200

    Add click event

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const gameBoard = (() => {
 const Tile = element => {
   let _state = '';
   const _element = element;
+  let _marked = false;
 
   const getState = () => _state;
 
@@ -21,10 +22,36 @@ const Tile = element => {
     _element.textContent = _state;
   };
 
+  _element.addEventListener('click', e => {
+    if (_marked) {
+      return;
+    }
+
+    setState(game.getCurrentPlayer().getMark());
+    game.nextTurn();
+    _marked = true;
+  });
+
   return { getState, setState };
 };
 
+const Player = mark => {
+  const getMark = () => {
+    return mark;
+  };
+
+  return { getMark };
+};
+
 const game = (() => {
+  let _currentPlayer = null;
+  const _players = [];
+  let _turn = 0;
+
+  const getCurrentPlayer = () => {
+    return _currentPlayer;
+  };
+
   const setupBoard = () => {
     const container = document.querySelector('.container');
     for (let i = 0; i < 9; i++) {
@@ -38,7 +65,22 @@ const game = (() => {
     }
   };
 
-  return { setupBoard };
+  const start = () => {
+    const p1 = Player('X');
+    _players.push(p1);
+    const p2 = Player('O');
+    _players.push(p2);
+
+    _currentPlayer = _players[0];
+  };
+
+  const nextTurn = () => {
+    _turn++;
+    _currentPlayer = _players[_turn % 2];
+  };
+
+  return { setupBoard, start, getCurrentPlayer, nextTurn };
 })();
 
 game.setupBoard();
+game.start();
